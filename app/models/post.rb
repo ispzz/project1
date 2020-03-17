cd6c241b688c9fae2c15b57bee6736fbea9fe328
Zara <zara@okaayfinee.home>
2020-03-17 15:14:40 +1100

    error msg won't show :(

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :user, :optional => true
+  validate :message_or_image_required
+
+  # def message_or_image_required
+  #   if message.blank? || image.blank?
+  #     flash[:alert] = "Need to include an image or message."
+  #   end
+  # end
+
+  def message_or_image_required
+    if message.blank?
+      errors.add(:message, "Must include a message")
+    elsif image.blank?
+      errors.add(:image, "Must include an image")
+    end
+  end
+
 end
