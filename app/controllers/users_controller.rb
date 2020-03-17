1bdbcba3ab88dfe681f0e6dedc351db73a610353
Zara <zara@okaayfinee.local>
2020-03-17 22:43:18 +1100

    added edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    user = User.find params[:id]
+    user.update user_params
+    redirect_to user
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :image)
