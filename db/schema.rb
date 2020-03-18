977334ddb3c209c5898c215cdf86baa3c927a3bb
Zara <zara@okaayfinee.local>
2020-03-19 08:27:10 +1100

    removed date from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_062715) do
+ActiveRecord::Schema.define(version: 2020_03_18_100253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_062715) do
   create_table "posts", force: :cascade do |t|
     t.text "message"
     t.text "image"
-    t.date "date"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
